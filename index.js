f207eb56c504991bec106cd1021706e1cd77c80c
haiqinJack <1198617472@qq.com>
2022-03-21 22:48:34 +0800

    add signin

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-var history = require("connect-history-api-fallback");
-var path = require("path");
+const history = require("connect-history-api-fallback");
+const cors = require("cors");
+const path = require("path");
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: ["GET", "PUT", "POST"],
+};
+
 const app = express();
+
+app.use(cors(corsOptions));
+
 app.use(history());
+
 // Register ejs as .html. If we did
 // not call this, we would need to
 // name our views foo.ejs instead
@@ -18,6 +29,8 @@ app.engine(".html", require("ejs").__express);
 app.set("views", path.join(__dirname, "dist"));
 // Path to our public directory
 app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // Without this you would need to
 // supply the extension to res.render()
 // ex: res.render('users.html').
@@ -27,6 +40,14 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.post("/signin", (req, res) => {
+    console.log(req.body, "req.body");
+    res.status(200);
+    res.json({
+        code: 0,
+        token: "testToken",
+    });
+});
 app.get("/xiafeng", (req, res) => {
     res.send("欢迎使用微信云托管！");
 });
