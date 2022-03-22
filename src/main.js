823c4c1768954e6c80e5a983c1cfffccf64ad5c3
haiqinJack <1198617472@qq.com>
2022-03-23 01:34:42 +0800

    ???

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
+
+// 原文链接：https://blog.csdn.net/weixin_40716786/article/details/90045802
+import "default-passive-events"; //Added non-passive event listener to a scroll-blocking 'mousewheel' event. Consider marking event handler as 'passive' to make the page more responsive.
 import "./assets/css/global.css";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
